7c76d39c476d4af1911d0598c860ecea63163a2c
Arcturusfr <arcturus_fr@hotmail.com>
2026-05-22 05:05:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // PhotoManuel — Service Worker
 // Version du cache
-const CACHE_NAME = 'photomanuel-v3';
+const CACHE_NAME = 'photomanuel-v4';
 
 // Fichiers à mettre en cache pour le mode hors-ligne
 const ASSETS = [
